1f9b703f7f303fdd6de9df8d4adbda2916d1fd22
Mauricio Gomez <gomezb_mauricio@hotmail.com>
2023-07-16 13:55:32 -0500

    project section

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,29 +1,30 @@
 import React from "react";
-import githubIcon from "../assets/icons/github.png";
-import linkedinIcon from "../assets/icons/linkedin.png";
+import githubIcon from "../assets/icons/gh-black.png";
 
-export default function Footer() {
+export default function Project(props) {
   return (
-    <div className="footer">
-      <div>
-        <a
-          href="https://github.com/MauricioGB1"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <img className="icon" src={githubIcon} alt="github icon" />
-        </a>
+    <div className="card px-0 project-card col-xl-3 col-lg-4 col-md-4 col-sm-6 col-8">
+      <div className="project-img ">
+        <img alt="project" className="card-img-top" src={props.image} />
       </div>
-      <div>
-        <a
-          href="https://www.linkedin.com/"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <img className="icon" src={linkedinIcon} alt="linkedin icon" />
-        </a>
+      <div className="card-body row">
+        <div className="col-sm-9 col-9">
+          <a
+            className="project-title"
+            href={props.deploy}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            {props.title}
+          </a>
+          <p className="project-descriiption">{props.descriiption}</p>
+        </div>
+        <div className="col-sm-3 col-3 github-div">
+          <a href={props.github} target="_blank" rel="noreferrer noopener">
+            <img className="github" src={githubIcon} alt="github icon" />
+          </a>
+        </div>
       </div>
-      <p className="copyright">©Made by Wenwen Tian</p>
     </div>
   );
 }
