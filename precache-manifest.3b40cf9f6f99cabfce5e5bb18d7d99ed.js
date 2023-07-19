9f1de879b858c662cc90147514e23c66d44ab990
Mauricio Gomez <gomezb_mauricio@hotmail.com>
2023-07-19 00:02:16 -0500

    Updates

---
+++ precache-manifest.3b40cf9f6f99cabfce5e5bb18d7d99ed.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "09f7f99abbe29f5059e6",
+    "url": "/MAUGB-M20C-React-Portfolio/static/css/main.26c9f1b6.chunk.css"
+  },
+  {
+    "revision": "09f7f99abbe29f5059e6",
+    "url": "/MAUGB-M20C-React-Portfolio/static/js/main.1a34785e.chunk.js"
+  },
+  {
+    "revision": "d271d5e52521e33689f6",
+    "url": "/MAUGB-M20C-React-Portfolio/static/js/runtime~main.98ce49f5.js"
+  },
+  {
+    "revision": "7185b4d4b28418bafbb1",
+    "url": "/MAUGB-M20C-React-Portfolio/static/css/2.91fcb39f.chunk.css"
+  },
+  {
+    "revision": "7185b4d4b28418bafbb1",
+    "url": "/MAUGB-M20C-React-Portfolio/static/js/2.59bfa30e.chunk.js"
+  },
+  {
+    "revision": "f26fd9fa1b38c72d1c0f",
+    "url": "/MAUGB-M20C-React-Portfolio/static/js/3.8aa940e2.chunk.js"
+  },
+  {
+    "revision": "eb3676f1b6f5f896049b",
+    "url": "/MAUGB-M20C-React-Portfolio/static/js/4.a6daf95e.chunk.js"
+  },
+  {
+    "revision": "e13aa50cff19e42a4e46d379998322d3",
+    "url": "/MAUGB-M20C-React-Portfolio/static/media/web.e13aa50c.jpg"
+  },
+  {
+    "revision": "724463c8ec541d889b4c8a93eadff849",
+    "url": "/MAUGB-M20C-React-Portfolio/static/media/linkedin.724463c8.jpg"
+  },
+  {
+    "revision": "6ec4c14980956299732f30bc36068538",
+    "url": "/MAUGB-M20C-React-Portfolio/static/media/project1.6ec4c149.jpg"
+  },
+  {
+    "revision": "218f59f17126595c7dc74d2ff3e66453",
+    "url": "/MAUGB-M20C-React-Portfolio/static/media/project2.218f59f1.jpg"
+  },
+  {
+    "revision": "00b8f4ef0983ffc8a9ff9c5581c6c5c2",
+    "url": "/MAUGB-M20C-React-Portfolio/static/media/project3.00b8f4ef.jpg"
+  },
+  {
+    "revision": "3f3f010441188eb56305551293d7f35d",
+    "url": "/MAUGB-M20C-React-Portfolio/static/media/project4.3f3f0104.jpg"
+  },
+  {
+    "revision": "bf692890d08bd3c6b9a6acdf94fde01a",
+    "url": "/MAUGB-M20C-React-Portfolio/static/media/project5.bf692890.jpg"
+  },
+  {
+    "revision": "3ae9373065c3209e6a4d8b8ecacad896",
+    "url": "/MAUGB-M20C-React-Portfolio/static/media/project6.3ae93730.jpg"
+  },
+  {
+    "revision": "607689f19afd054c731e0b2832d3a0b5",
+    "url": "/MAUGB-M20C-React-Portfolio/static/media/error.607689f1.jpg"
+  },
+  {
+    "revision": "a97ce4dfdda412ff73af6fde57e6132b",
+    "url": "/MAUGB-M20C-React-Portfolio/static/media/send.a97ce4df.jpg"
+  },
+  {
+    "revision": "d69c5b7d4a7ec003dc0ec5d8827bde9b",
+    "url": "/MAUGB-M20C-React-Portfolio/static/media/Resume.d69c5b7d.pdf"
+  },
+  {
+    "revision": "d37b460fd871d5982604c2137f7a4d01",
+    "url": "/MAUGB-M20C-React-Portfolio/static/media/download.d37b460f.jpg"
+  },
+  {
+    "revision": "7c087475695031cfb99d89816fe0c7cc",
+    "url": "/MAUGB-M20C-React-Portfolio/index.html"
+  }
+];
